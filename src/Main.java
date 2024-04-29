d98a3d6e83b3be8372b9245f77b420c4bed218ff
Paulo <paulompj010@gmail.com>
2024-04-29 12:39:24 -0300

    git status

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Digite sua altura:");
+        System.out.println("Digite sua altura:");
+        System.out.println("Digite sua altura:");
+
+    }
+
+}
