cc006ddb68c994895171e3528cdcb22ec15d1e7b
Paulo <paulompj010@gmail.com>
2024-04-30 19:59:13 -0300

    Exercicio float,int

---
+++ src/SOMA.java
@@ -0,0 +1,11 @@
+public class SOMA {
+    public static void main(String[] args) {
+        float salario;
+        salario=700f;
+        int guardaSalario;
+        guardaSalario=(int) salario;
+        System.out.printf("Seu salário é: %d\n", guardaSalario);
+        System.out.printf("Seu salário é: %f", salario);
+
+    }
+}
