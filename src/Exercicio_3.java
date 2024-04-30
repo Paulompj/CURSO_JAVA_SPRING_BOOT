603e052ba726e37cc00751a57eb546d485cd0910
Paulo <paulompj010@gmail.com>
2024-04-30 13:49:18 -0300

    Exercicio 3

---
+++ src/Exercicio_3.java
@@ -0,0 +1,12 @@
+public class Exercicio_3 {
+    public static void main(String[] args) {
+        //Calculando area de um trapézio//
+        double base_maior,altura,base_menor,area;
+        base_maior=8.0;
+        base_menor=6.0;
+        altura=5.0;
+        area=(base_menor+base_maior)*altura/2.0;
+        System.out.printf("A area desse trapézio é: %.3f",area);
+    }
+}
+
