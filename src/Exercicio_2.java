12aa03d67bae88388ddaea87e0f36eb4a3ff7ff1
Paulo <paulompj010@gmail.com>
2024-04-30 13:48:47 -0300

    Segundo exercicio

---
+++ src/Exercicio_2.java
@@ -0,0 +1,10 @@
+public class Exercicio_2 {
+    public static void main(String[] args) {
+        int num1,num2,soma;
+        num1=5;
+        num2=6;
+        soma=num1+num2;
+        System.out.printf("%d + %d = %d",num1,num2,soma);
+
+    }
+}
