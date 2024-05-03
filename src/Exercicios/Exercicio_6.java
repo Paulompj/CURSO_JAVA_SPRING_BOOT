b89aebd2ec34eceb0f78a64b97be927f71052858
Paulo <paulompj010@gmail.com>
2024-05-03 10:06:20 -0300

    Programa Posto de Gasolina

---
+++ src/Exercicios/Exercicio_6.java
@@ -0,0 +1,38 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int escolhaMenu,gasolina = 0,diesel = 0,alcool = 0;
+        System.out.println("1 - Gasolina");
+        System.out.println("2 - Diesel");
+        System.out.println("3 - Álcool");
+        System.out.println("4 - Sair");
+        System.out.print("Escolha: ");
+        escolhaMenu = input.nextInt();
+        while(escolhaMenu != 4){
+            if(escolhaMenu == 1){
+                gasolina+=1;
+            }
+            else if(escolhaMenu == 2){
+                diesel+=1;
+            }
+            else if(escolhaMenu == 3){
+                alcool+=1;
+            }
+            System.out.println("1 - Gasolina");
+            System.out.println("2 - Diesel");
+            System.out.println("3 - Álcool");
+            System.out.println("4 - Sair");
+            System.out.print("Escolha: ");
+            escolhaMenu = input.nextInt();
+        }
+        System.out.println("Ficha de dados:");
+        System.out.printf("Gasolina: %dL\n",gasolina);
+        System.out.printf("Diesel: %dL\n",diesel);
+        System.out.printf("Álcool: %dL",alcool);
+        input.close();
+    }
+}
