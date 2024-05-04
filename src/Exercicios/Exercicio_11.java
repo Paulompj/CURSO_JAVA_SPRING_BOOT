a4da8abc5e69077e20d2addf71124ef60d69a3fe
Paulo <paulompj010@gmail.com>
2024-05-04 18:48:21 -0300

    Formatação com println

---
+++ src/Exercicios/Exercicio_11.java
@@ -0,0 +1,9 @@
+package Exercicios;
+
+public class Exercicio_11 {
+    public static void main(String[] args) {
+        int a=10;
+        String nome="Paulo";
+        System.out.println("Seu nome: "+nome+"Bomdia");
+    }
+}
