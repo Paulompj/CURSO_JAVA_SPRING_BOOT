157f790133f397b88d85e82bbf07a4d76cf567a6
Paulo <paulompj010@gmail.com>
2024-05-04 21:54:10 -0300

    Iniciando na orientação a objetos - Classes e Objetos

---
+++ src/Entidades/Triangulo.java
@@ -0,0 +1,10 @@
+package Entidades;
+
+public class Triangulo {
+    public double a;
+    public double b;
+    public double c;
+
+
+
+}
--- src/Ex_POO.java
+++ src/Ex_POO.java
@@ -1,3 +1,5 @@
+import Entidades.Triangulo;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -5,19 +7,22 @@ public class Ex_POO {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
-        double xA,xB,xC,yA,yB,yC,areaX,areaY,px,py;
+        Triangulo x,y;
+         x = new Triangulo();
+         y = new Triangulo();
+
         System.out.println("Digite as medidas dos lados do triangulo X:");
-        xA = input.nextDouble();
-        xB = input.nextDouble();
-        xC = input.nextDouble();
+        x.a = input.nextDouble();
+        x.a = input.nextDouble();
+        x.a = input.nextDouble();
         System.out.println("Digite as medidas dos lados do triangulo y:");
-        yA = input.nextDouble();
-        yB = input.nextDouble();
-        yC = input.nextDouble();
-        px = (xA+xB+xC)/2;
-        py = (yA+yB+yC)/2;
-        areaX = Math.sqrt(px*(px-xA)*(px-xB)*(px-xC));
-        areaY = Math.sqrt(py*(py-yA)*(py-yB)*(py-yC));
+        y.a = input.nextDouble();
+        y.b = input.nextDouble();
+        y.c = input.nextDouble();
+        double px = (x.a+x.b+x.c)/2;
+        double py = (y.a+y.b+y.c)/2;
+        double areaX = Math.sqrt(px*(px-x.a)*(px-x.b)*(px-x.c));
+        double areaY = Math.sqrt(py*(py-y.a)*(py-y.b)*(py-y.c));
         System.out.println("A area do triângulo x é: "+areaX);
         System.out.println("A area do triângulo y é: "+areaY);
         if(areaX>areaY){
