b631764981c4757a2626cb5de22d8052475f639d
Paulo <paulompj010@gmail.com>
2024-05-07 19:09:52 -0300

    Classe Funcionario do programa Salário funcionario

---
+++ src/Entidades/Funcionario.java
@@ -0,0 +1,20 @@
+package Entidades;
+
+public class Funcionario {
+    public String nome;
+    public double salarioBruto;
+    public double imposto;
+    public double SalarioLiquido(){
+        return salarioBruto - imposto;
+    }
+    public void AumentarSalario(double x){
+        salarioBruto = salarioBruto +(salarioBruto * x/100);
+    }
+    public String toString(){
+        return "\nFicha do Funcionário:\n"+
+                "Nome: "+nome+"\n"+
+                "Salario Bruto: "+salarioBruto+"\n"+
+                "Salario Líquido: "+SalarioLiquido();
+
+    }
+}
