1f7980b50b04815a42a04f5a98081abc823fa95c
Paulo <paulompj010@gmail.com>
2024-05-09 21:11:42 -0300

    Modificação

---
+++ src/Teste.java
@@ -0,0 +1,8 @@
+import Entidades.Product;
+
+public class Teste {
+    public static void main(String[] args) {
+        Product produto1 = new Product();
+        System.out.println(produto1);
+    }
+}
