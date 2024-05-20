b30d8e90f025e505fdc342374a5ea96226c5baf1
Paulo <paulompj010@gmail.com>
2024-05-20 19:16:26 -0300

    Exemplo 1

---
+++ src/Exercicios2/Ex1.java
@@ -0,0 +1,34 @@
+/*package Exercicios2;
+
+import java.util.ArrayList;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        /*System.out.println("------------------TIPO PRIMITIVO------------------");
+        System.out.println("------------ArrayList----------");
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        lista.add(3);
+        System.out.println(lista);
+        System.out.println(lista.get(0));
+        System.out.println("------------Vetor----------");
+        int[] vetorInteiro = new int[3];
+        System.out.println(vetorInteiro);
+        System.out.println(vetorInteiro[0]);*/
+
+        /*System.out.println("\n------------------CLASSES-----------------------");
+        System.out.println("------------ArrayList----------");
+        ArrayList<Pessoa> lista2 = new ArrayList<Pessoa>();
+        lista2.add(new Pessoa("Paulo"));
+        System.out.println(lista2);
+        System.out.println(lista2.get(0));
+        System.out.println("------------Vetor----------");
+        Pessoa[] vetorPessoa = new Pessoa[3];
+        vetorPessoa[0] = new Pessoa("Paulo");
+        System.out.println(vetorPessoa);
+        System.out.println(vetorPessoa[0]);
+        //lista2.get(0) -> nome da gaveta//
+        //vetor[0] -> nome da gaveta//
+
+
+    }
+}*/
