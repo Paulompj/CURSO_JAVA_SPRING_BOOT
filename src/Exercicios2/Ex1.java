7709cac7dbacf14a763942894ecf90b606c764ef
Paulo <paulompj010@gmail.com>
2024-05-29 12:05:59 -0300

    Diferença de arraylis tipos primitivos classes vetores

--- src/Exercicios2/Ex1.java
+++ src/Exercicios2/Ex1.java
@@ -1,10 +1,10 @@
-/*package Exercicios2;
+package Exercicios2;
 
 import java.util.ArrayList;
 
 public class Ex1 {
     public static void main(String[] args) {
-        /*System.out.println("------------------TIPO PRIMITIVO------------------");
+        System.out.println("------------------TIPO PRIMITIVO------------------");
         System.out.println("------------ArrayList----------");
         ArrayList<Integer> lista = new ArrayList<Integer>();
         lista.add(3);
@@ -13,9 +13,9 @@ public class Ex1 {
         System.out.println("------------Vetor----------");
         int[] vetorInteiro = new int[3];
         System.out.println(vetorInteiro);
-        System.out.println(vetorInteiro[0]);*/
+        System.out.println(vetorInteiro[0]);
 
-        /*System.out.println("\n------------------CLASSES-----------------------");
+        System.out.println("\n------------------CLASSES-----------------------");
         System.out.println("------------ArrayList----------");
         ArrayList<Pessoa> lista2 = new ArrayList<Pessoa>();
         lista2.add(new Pessoa("Paulo"));
@@ -31,4 +31,4 @@ public class Ex1 {
 
 
     }
-}*/
+}
