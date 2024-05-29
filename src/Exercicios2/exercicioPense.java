97f377c7b20ec4392bf1bd0e0ecc3f31be1c198c
Paulo <paulompj010@gmail.com>
2024-05-29 12:03:15 -0300

    Exercicio figuras geométricas no console

---
+++ src/Exercicios2/exercicioPense.java
@@ -0,0 +1,50 @@
+package Exercicios2;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicioPense {
+    public static void main(String[] args) {
+        /*Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+        String nome = input.nextLine();
+        LocalDateTime datahora = LocalDateTime.now();
+        SecondAluno p = new SecondAluno(nome,datahora);
+        System.out.println(p);
+        input.close();*/
+        //for (int i = 0; i<40; i+=1){
+            /*if(i <= 9){
+            System.out.print("*\t");
+            }
+            else if(i>=10 && i<=20){
+                System.out.println("*\t\t\t\t\t\t\t\t\t\t*");
+            }
+            else if(i>15 && i<=31){
+                System.out.print("*\t");
+            }*/
+        /* QUADRADO System.out.print("* * * * * * *");
+        System.out.println();
+        System.out.print("*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n*\t\t\t*\n");
+        System.out.print("* * * * * * *");*/
+        /* ELIPSE System.out.print("\t****\n  *\t     *\n  *\t     *\n  *\t     *\n\t****");*/
+        /*Espada System.out.println("\t*");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("    *");
+        System.out.println("    *");
+        */
+        //System.out.println("\t\t*\n********\t*\n\t\t*");
+
+
+
+
+
+    }
+
+    }
+
