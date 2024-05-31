ced1785ff339ebfd9948807443b69e57de149ec1
Paulo <paulompj010@gmail.com>
2024-05-31 11:58:00 -0300

    Testando a fatura em java

---
+++ src/Exercicios2/TesteFatura.java
@@ -0,0 +1,9 @@
+package Exercicios2;
+
+public class TesteFatura {
+    public static void main(String[] args) {
+        Fatura Bola = new Fatura("BOLA","O MELHOR AMIGO DO HOMEM",2,5);
+        System.out.println(Bola);
+
+    }
+}
