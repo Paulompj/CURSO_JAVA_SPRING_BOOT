2335d96be7884ec4a745617c2bfdb37ce87d138c
Paulo <paulompj010@gmail.com>
2024-06-01 10:21:22 -0300

    Testando classe Date

---
+++ src/Exercicios2/DateTeste.java
@@ -0,0 +1,8 @@
+package Exercicios2;
+
+public class DateTeste {
+    public static void main(String[] args) {
+        Date p1 = new Date(1,06,2024);
+        System.out.println(p1.displayDate());
+    }
+}
