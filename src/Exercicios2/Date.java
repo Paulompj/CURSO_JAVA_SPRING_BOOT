56b62c377b5dc009a4fcda26837b920333e08247
Paulo <paulompj010@gmail.com>
2024-06-01 10:20:42 -0300

    Classe data -> Simulando a classe do java LocalDate

---
+++ src/Exercicios2/Date.java
@@ -0,0 +1,39 @@
+package Exercicios2;
+
+public class Date {
+    private int dia;
+    private int mes;
+    private int ano;
+    public Date(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public String displayDate(){
+        return String.format("%d/%d/%d",dia,mes,ano);
+    }
+}
