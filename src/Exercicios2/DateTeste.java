ef6f30bfe57e94c51f6300c67f259d10c4104eea
Paulo <paulompj010@gmail.com>
2024-06-03 12:07:28 -0300

    Testando atuaalização na classe Date

--- src/Exercicios2/DateTeste.java
+++ src/Exercicios2/DateTeste.java
@@ -4,5 +4,7 @@ public class DateTeste {
     public static void main(String[] args) {
         Date p1 = new Date(1,06,2024);
         System.out.println(p1);
+        Date n = new Date();
+        System.out.println(n);
     }
 }
