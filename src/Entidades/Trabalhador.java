3789519b40c4a52459cc588b6cc0a31833336a54
Paulo <paulompj010@gmail.com>
2024-06-04 22:00:40 -0300

    Classe Trabalhador -> É o funcionario do programa, implementa as classes Contrato, Nivel de trabalho e data

---
+++ src/Entidades/Trabalhador.java
@@ -0,0 +1,55 @@
+package Entidades;
+
+import Entidades.Enums.Nivel_Trabalho;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Trabalhador {
+    private String nome;
+    private Nivel_Trabalho nivelTrabalho;
+    private double baseSalarial;
+    private int codigoNivelTrabalho;
+    private Departamento departamento;
+    ArrayList<Contrato_Por_Hora> Contratos = new ArrayList<>();
+
+    public Trabalhador(String nome, Nivel_Trabalho nivelTrabalho, double baseSalarial, Departamento departamento, Contrato_Por_Hora x){
+        this.nome = nome;
+        this.nivelTrabalho = nivelTrabalho;
+        this.codigoNivelTrabalho = nivelTrabalho.getCodigoNivelTrabalho();
+        this.baseSalarial = baseSalarial;
+        this.departamento = departamento;
+        Contratos.add(new Contrato_Por_Hora(x.getData(),x.getValorPorHora(),x.getHorasTrabalhadas()));
+    }
+    public void AdicionarContrato(LocalDate data, double valorPorHora, double horasTrabalhadas){
+        Contratos.add(new Contrato_Por_Hora(data,valorPorHora,horasTrabalhadas));
+    }
+    public boolean ContratoCheioVazio(){
+        if (Contratos.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void VisualizarContratos(){
+        if(Contratos.isEmpty()){
+            System.out.println("Nenhum contrato vigente");
+        }
+        else{
+            for(int i = 0; i < Contratos.size(); i+=1){
+                System.out.printf("%nContrato %d:\n%s%n",i+1,Contratos.get(i)).toString();
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Trabalhador" +"\n"+
+                "Nome = " + nome +"\n"+
+                "NivelTrabalho = " + nivelTrabalho +"\n"+
+                "BaseSalarial = " + baseSalarial +"\n"+
+                "CodigoNivelTrabalho = " + codigoNivelTrabalho +"\n"+
+                departamento+"\n";
+    }
+}
