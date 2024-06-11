f557176cb07de7d5a5e43f7b0c562ca84b95ed0d
Paulo <paulompj010@gmail.com>
2024-06-10 21:58:57 -0300

    Teste

---
+++ src/Exercicios2/teste222.java
@@ -0,0 +1,18 @@
+package Exercicios2;
+
+import Exercicios2.ExercicioComposição.StatusPedido;
+
+public class teste222 {
+    public static void main(String[] args) {
+        //String[] x = {"Paulo","Moraes","Pessoa","Jardim"};
+        String[] x = {"Ola, ", "Mundo. ","Tudo bem com vocês."};
+        StringBuilder y = new StringBuilder();
+        for (String s : x){
+            y.append(s);
+        }
+        System.out.println(y);
+        int precos[] = new int[5];
+
+
+    }
+}
