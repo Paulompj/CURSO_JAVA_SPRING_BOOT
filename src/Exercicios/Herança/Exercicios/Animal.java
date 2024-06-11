08c49dbdb5b91a0d57cccac09aebdfdcaa7a4262
Paulo <paulompj010@gmail.com>
2024-06-11 11:24:45 -0300

    Classe Animal

---
+++ src/Exercicios/Herança/Exercicios/Animal.java
@@ -0,0 +1,7 @@
+package Exercicios.Herança.Exercicios;
+
+public class Animal {
+    private String nome;
+    public Animal(String nome){this.nome = nome;};
+    public String barulho(){return "Eu sou um animal";};
+}
