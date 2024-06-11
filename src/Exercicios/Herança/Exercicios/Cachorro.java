c316a48b89b7742b5b9aa49b9871f70761268681
Paulo <paulompj010@gmail.com>
2024-06-11 19:18:36 -0300

    Classe Cachorro que herda a classe Animal --> Testando Herança

--- src/Exercicios/Herança/Exercicios/Cachorro.java
+++ src/Exercicios/Herança/Exercicios/Cachorro.java
@@ -1,10 +1,10 @@
 package Exercicios.Herança.Exercicios;
 
 public class Cachorro extends Animal{
-
     public Cachorro(String nome) {
         super(nome);
     }
+    @Override
     public String barulho(){return "AU AU AU";};
     public void BalançarRabo(){ };
 }
