b3a8638d7c3f046caec434aa3b176332237451fe
Paulo <paulompj010@gmail.com>
2024-06-11 10:31:15 -0300

    Testando a herança

---
+++ src/Exercicios/TesteHerança.java
@@ -0,0 +1,15 @@
+package Exercicios;
+
+
+public class TesteHerança {
+    public static void main(String[] args) {
+        Conta conta = new ContaEmpresa("Paulo",432,400.0,10000.0);
+
+        /*Conta conta = new ContaEmpresa("Paulo",344,500.0,10.000);
+        ContaEmpresa Contaemp = (ContaEmpresa) conta;
+        System.out.println(Contaemp.toString());*/
+
+    }
+
+
+}
