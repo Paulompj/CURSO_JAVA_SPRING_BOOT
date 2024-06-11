3b5c6639d47940fcf5aec568500f83bb72b684df
Paulo <paulompj010@gmail.com>
2024-06-11 11:25:20 -0300

    Classe Cachorro extends Animal

---
+++ src/Exercicios/Herança/Exercicios/Cachorro.java
@@ -0,0 +1,10 @@
+package Exercicios.Herança.Exercicios;
+
+public class Cachorro extends Animal{
+
+    public Cachorro(String nome) {
+        super(nome);
+    }
+    public String barulho(){return "AU AU AU";};
+    public void BalançarRabo(){ };
+}
