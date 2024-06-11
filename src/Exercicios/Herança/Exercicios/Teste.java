47c1ab214038e5016124fe4179a50a3aa66679ef
Paulo <paulompj010@gmail.com>
2024-06-11 11:26:55 -0300

    Upcasting e DownCasting Testando

---
+++ src/Exercicios/Herança/Exercicios/Teste.java
@@ -0,0 +1,21 @@
+package Exercicios.Herança.Exercicios;
+
+import java.time.LocalDate;
+
+public class Teste {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal");
+        //System.out.println(animal.barulho());
+        Cachorro pink = new Cachorro("Pink");
+        //System.out.println(pink.barulho());
+        Animal x = new Cachorro("Dogao");
+        System.out.println(Animal(pink));
+    }
+    public static String Animal(Animal y){
+        if (y instanceof Cachorro){
+            return "É CACHORRO";
+        }
+        return "É animal";
+    }
+}
+
