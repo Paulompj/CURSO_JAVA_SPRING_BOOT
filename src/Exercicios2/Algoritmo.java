6606520b58f3bb6adc831ff0b661a20d78bbbf5a
Paulo <paulompj010@gmail.com>
2024-06-13 12:30:08 -0300

    Classe estática para descobrir os numeros de um número e sua quantidade de algarismos

--- src/Exercicios2/Algoritmo.java
+++ src/Exercicios2/Algoritmo.java
@@ -1,11 +1,16 @@
 package Exercicios2;
 
+import java.util.ArrayList;
+
 public class Algoritmo {
     static int numExecucoes = 0;
+    public static ArrayList<Integer> algoritmos = new ArrayList<>();
     public static int tamNum(int numero){
         for(numExecucoes = 0; numero > 10;numExecucoes+=1){
+            algoritmos.add(numero%10);
             numero = numero/10;
         }
+        algoritmos.add(numero%10);
         numExecucoes+=1;
         return numExecucoes;
     }
