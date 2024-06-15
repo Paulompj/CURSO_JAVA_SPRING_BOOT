2ce978e9d9fe8e8de062358eeb5fd05b3af7868c
Paulo <paulompj010@gmail.com>
2024-06-15 20:47:18 -0300

    Enum Cor --> Implementa Cores(BLACK, YELLOW, WHITE E RED)

---
+++ src/Exercicios/Herança/Exercicios/Exercicios3/Cor.java
@@ -0,0 +1,9 @@
+package Exercicios.Herança.Exercicios.Exercicios3;
+
+public enum Cor {
+    BLACK(0),RED(1),YELLOW(2),WHITE(3);
+    protected int codigoCor;
+    Cor(int i){
+        this.codigoCor = i;
+    }
+}
