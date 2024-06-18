608346888eba514b189e6405f0a59fdeb718f8de
Paulo <paulompj010@gmail.com>
2024-06-18 20:49:14 -0300

    Aplicação testando Classe Reserva

---
+++ src/Exercicios/Excecoes/teste.java
@@ -0,0 +1,41 @@
+package Exercicios.Excecoes;
+
+import Exercicios.Excecoes.Exercicio1.Reserva;
+
+import javax.swing.text.DateFormatter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.logging.SimpleFormatter;
+
+public class teste {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        SimpleDateFormat formatacao = new SimpleDateFormat("dd/MM/yyyy");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite o numero do quarto: ");
+        int numQuarto = input.nextInt();
+        input.nextLine();
+        System.out.println("Digite a data de ENTRADA:");
+        Date entrada = formatacao.parse(input.nextLine());
+        System.out.println("Digite a data de SAÍDA:");
+        Date saida = formatacao.parse(input.nextLine());
+        if (entrada.after(saida)){
+            System.out.println("Erro! Entrada esta depois da saída");
+        }
+        else{
+            Reserva quarto1 = new Reserva(numQuarto,entrada,saida);
+            System.out.println(quarto1);
+        }
+
+
+
+
+        input.close();
+}
+}
