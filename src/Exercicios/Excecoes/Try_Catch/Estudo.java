f706d09f83ad35feb04c49d3d43ba0047f737d03
Paulo <paulompj010@gmail.com>
2024-06-21 20:07:24 -0300

    Estudando Tratamento de exceções

--- src/Exercicios/Excecoes/Try_Catch/Estudo.java
+++ src/Exercicios/Excecoes/Try_Catch/Estudo.java
@@ -1,2 +1,23 @@
-package Exercicios.Excecoes.Try_Catch;public class Estudo {
+package Exercicios.Excecoes.Try_Catch;
+public class Estudo {
+    public static void main(String[] args) {
+        int vet[] = {1, 2, 0, 4, 5};
+        int soma = 0;
+        for(int i = 0; i < vet.length; i += 1) {
+            try {
+                if((i+1)%2 != 0){
+                    throw new DivisaoException();
+                }
+                soma += i + 1 / vet[i];
+                System.out.println("OK");
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Esse array nao existe");
+            } catch (ArithmeticException e) {
+                System.out.println("DIVISAO POR ZERO NAO PODE!!");
+            } catch (DivisaoException x){
+                System.out.println(x.getMessage());
+            }
+        }
+    }
 }
