dd5719d267f3e2357bd0331c3bc57232ec83eca5
Paulo <paulompj010@gmail.com>
2024-06-25 19:09:31 -0300

    Hackerrank Java01

---
+++ src/Exercicios/Arquivos/Estudo/E01.java
@@ -0,0 +1,29 @@
+package Exercicios.Arquivos.Estudo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+//LENDO OS DADOS DO TXT//
+public class E01 {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\PC\\Desktop\\in.txt");
+        Scanner input = null;
+        try{
+            input = new Scanner(file);
+            while(input.hasNextLine()){
+                System.out.println(input.nextLine());
+            }
+
+        } catch (IOException e) {
+            System.err.println("Erro: "+e.getMessage());
+            e.printStackTrace();
+        }
+        finally {
+            if(input != null){
+                input.close();
+            }
+        }
+    }
+}
