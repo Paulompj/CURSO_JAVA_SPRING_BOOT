e6a591924de1917225e07fbbf16e3859ed26d41c
Paulo <paulompj010@gmail.com>
2024-07-02 11:03:19 -0300

    Classe Filme

---
+++ src/Exercicios/Arquivos/Estudo/CSV/ExFilmes/Filme.java
@@ -0,0 +1,39 @@
+package Exercicios.Arquivos.Estudo.CSV.ExFilmes;
+
+public class Filme {
+    private String nome;
+    private double nota;
+    private boolean assistindo;
+
+    // Métodos construtor
+    public Filme(String nome, double nota, boolean assistindo) {
+        this.nome = nome;
+        this.nota = nota;
+        this.assistindo = assistindo;
+    }
+
+    // Métodos Get e Set
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public boolean isAssistindo() {
+        return assistindo;
+    }
+
+    public void setAssistindo(boolean assistindo) {
+        this.assistindo = assistindo;
+    }
+}
