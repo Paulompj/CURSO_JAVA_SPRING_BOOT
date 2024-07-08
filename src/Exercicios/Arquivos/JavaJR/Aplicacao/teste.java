fdadd9911d2236825d46d1b510b21c83deb67d87
Paulo <paulompj010@gmail.com>
2024-07-08 11:47:51 -0300

    Teste

---
+++ src/Exercicios/Arquivos/JavaJR/Aplicacao/teste.java
@@ -0,0 +1,20 @@
+package Exercicios.Arquivos.JavaJR.Aplicacao;
+
+import Exercicios.Arquivos.JavaJR.Entidades.TXT;
+import Exercicios.Arquivos.JavaJR.Entidades.Usuario;
+import Exercicios.Arquivos.JavaJR.Entidades.views;
+import Exercicios.Arquivos.JavaJR.Exception.EmailException;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String[] args) {
+        String o = "Karius@gmail.com";
+        int x = o.indexOf('@');
+        System.out.println(x);
+
+    }
+}
