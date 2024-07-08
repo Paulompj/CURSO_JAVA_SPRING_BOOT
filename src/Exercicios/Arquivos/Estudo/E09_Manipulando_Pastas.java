05cedbec8b7c5754f9f104df1e88e8524d21e39d
Paulo <paulompj010@gmail.com>
2024-07-08 11:46:49 -0300

    Manipulando arquivos de pastas

--- src/Exercicios/Arquivos/Estudo/E09_Manipulando_Pastas.java
+++ src/Exercicios/Arquivos/Estudo/E09_Manipulando_Pastas.java
@@ -1,22 +1,22 @@
 package Exercicios.Arquivos.Estudo;
 
+import Exercicios.Arquivos.JavaJR.Entidades.views;
+
 import java.io.File;
 
 public class E09_Manipulando_Pastas {
     public static void main(String[] args) {
         File caminho = new File("C:\\Users\\PC\\Desktop");
         //Visualizando Diretórios//
-        System.out.println("\tDIRETÓRIOS E FILES");
+        /*System.out.println("\tDIRETÓRIOS E FILES");
         for(File x: caminho.listFiles()){
             System.out.println(x);
-        }
+        }*/
         System.out.println("\tAPENAS FILES");
-        for(File x: caminho.listFiles(File::isFile)){
-            System.out.println(x);
-        }
-        System.out.println("\tAPENAS DIRETÓRIOS");
+        views.listarUsuarios();
+        /*System.out.println("\tAPENAS DIRETÓRIOS");
         for(File x: caminho.listFiles(File::isDirectory)){
             System.out.println(x);
-        }
+        }*/
     }
 }
