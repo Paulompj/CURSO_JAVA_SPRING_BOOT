4b7fde1b6273130c90ef6b278b3e6fb8f5115282
Paulo <paulompj010@gmail.com>
2024-07-17 09:33:43 -0300

    Data

---
+++ src/Exercicios/Interfaces/Exercicio/Data/teste.java
@@ -0,0 +1,17 @@
+package Exercicios.Interfaces.Exercicio.Data;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class teste {
+    public static void main(String[] args) {
+        Data date = new Data();
+        System.out.println(date);
+        Data y = new Data("08/07/2024 13:35");
+        System.out.println(y);
+        
+
+
+
+    }
+}
